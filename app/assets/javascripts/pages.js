af0c5ca911b7391e18eb863ba49eb972e6fe9642
Yanina Mosilevich <y.mosilevich@gmail.com>
2016-08-11 15:00:35 +0300

    Cleaned pages.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,75 +1,39 @@
 function initializeSortable(){ 
-$('.draggable').draggable({
+	$('.draggable').draggable({
 			cancel: false,
 			revert: "invalid",
     	helper: function(e) {
-	    	var type = this.id
-	    	if (type == "picture") {
-	    		console.log("!!!!!", this)
-	    		return $(this).addClass('dragbox');
-
-	    		//  var innerTag = $('<div>').addClass('edit_area')
-		    	// innerTag.attr("title", "Click to edit")
-		    	// innerTag.text("New " + type)	    	
-		    } else {
-		    	var innerTag = $('<div>').addClass('edit_area')
-		    	innerTag.attr("title", "Click to edit")
-		    	innerTag.text("New " + type)
-	    	}
-	    	var res = $('<div>').addClass('dragbox').append(innerTag);
-	    	if (type == "text-btn") {
-	    		res.data("type", "markdown_text")
-	    	} else if (type == "video-btn") {
-	    		res.data("type", "video")
-	    	} else if (type == "picture") {
-	    		res.data("type", "picture")
-	    	}
-
-	    	return res;
-
-    },
-    stop: function( event, ui ) {
-     	if(this.id == "picture" && $(this).data("id") == "new"){
-     		cloudinary.openUploadWidget({ cloud_name: 'dg1a2dx9d', upload_preset: 'zothj6or', 'folder': 'user_photos' },
-	      function(error, result) {
-	      	var url = result[0]['url']
-	      	var curItem = $(ui.helper)
-	      	var innerTag = $('<img>').attr("src", url)
-	      	innerTag.data("public", result[0]['public_id'])	      	
-	      	curItem.html(innerTag)
-	      	console.log(curItem)
-	      	$(curItem).data("id", null)
-	      	console.log($(curItem).data("id"))
-	      	console.log($('#picture-btn').popover('hide'))
-	      	//innerTag.cloudinary();
-	      });
-     	}
-     	
-     	//if(this.id == "picture") $('#choosePictureModel').modal('show')  
-    },
-    connectToSortable: "div[class*='column']"
+	    	return getDraggableElement(this);
+    	},
+	    stop: function( event, ui ) {
+	     	if(this.id == "picture" && $(this).data("id") == "new"){
+	     		showUploadDialog(this);
+	     	}
+	    },
+    	connectToSortable: "div[class*='column']"
 	});
-	$("div[class*='column']").sortable({  
-    connectWith: "div[class*='column']",    
-    cursor: 'move',  
-    placeholder: 'placeholder',  
-    forcePlaceholderSize: true,  
-    opacity: 0.4, 
-    update: function(event, ui){  
-    	ui.item.attr('style','');
-    	pageLoad();
 
-    },
-    stop: function(event, ui){  
-      //updateWidgetData();
-    },
+	$("div[class*='column']").sortable({  
+	    connectWith: "div[class*='column']",    
+	    cursor: 'move',  
+	    placeholder: 'placeholder',  
+	    forcePlaceholderSize: true,  
+	    opacity: 0.4, 
+	    update: function(event, ui){  
+	    	ui.item.attr('style','');
+	    	initializeEditable();
+
+	    },
+	    stop: function(event, ui){  
+	      //updateWidgetData();
+	    },
 
 	})  
 	.disableSelection(); 
 }
 
 
- function pageLoad() {
+ function initializeEditable() {
   	//jquery delegate
  //  	$("body").on("click",".editable",function(e){
 
@@ -88,10 +52,8 @@ $('.draggable').draggable({
  // })
     $('.edit_area').editable(function(value, settings) {
 	    	var parent = $(this).parent();
-	    	if(parent.data("type") == "video" && value.includes("youtube")) {
-	    		parent.empty();
-	    		var frame = $('<iframe>').attr("src", value).attr("frameborder", 0).attr("allowfullscreen","");
-	    		parent.append(frame)
+	    	if(parent.data("type") == "video") {
+	    		loadVideo(parent, value)
 	    	}  	
 	    	return(value);
 	  	}, {
@@ -101,4 +63,45 @@ $('.draggable').draggable({
 	    		//updateWidgetData();
 	    }
     }); 
-  }
+}
+
+function loadVideo(parent, value) {
+	if (value.includes("youtube")) {
+		parent.empty();
+		var frame = $('<iframe>').attr("src", value).attr("frameborder", 0).attr("allowfullscreen","");
+		parent.append(frame);
+	}
+
+}
+
+function getDraggableElement(elem) {
+	var type = elem.id
+	if (type == "picture") {
+		return $(elem).addClass('dragbox');
+
+    }
+	var innerTag = $('<div>').addClass('edit_area')
+	innerTag.attr("title", "Click to edit")
+	innerTag.text("New " + type)
+
+	var res = $('<div>').addClass('dragbox').append(innerTag);
+	if (type == "text-btn") {
+		res.data("type", "markdown_text")
+	} else if (type == "video-btn") {
+		res.data("type", "video")
+	}
+	return res;
+}
+
+function showUploadDialog(elem) {
+	cloudinary.openUploadWidget({ cloud_name: 'dg1a2dx9d', upload_preset: 'zothj6or', 'folder': 'user_photos' },
+      function(error, result) {
+      	var url = result[0]['url']
+      	var curItem = $(elem)
+      	var innerTag = $('<img>').attr("src", url)
+      	innerTag.data("public", result[0]['public_id'])	      	
+      	curItem.html(innerTag)
+      	$(curItem).data("id", null)
+    });
+
+}
